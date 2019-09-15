dfbd8768d2ef6c590371b0342b0f24a371fbe684
Peter Li <peter.s.li93@gmail.com>
2019-09-15 15:43:23 -0400

    take input

--- project/runtime.py
+++ project/runtime.py
@@ -2,14 +2,16 @@ from PIL import Image
 import torch, json
 from torchvision import transforms, utils
 import numpy as np
+import os
 
 model = torch.load('model.pt')
 model.eval()
 
-def run(model, input):
+def run(input_name):
+    input_dir = os.path.join(os.getcwd(),'static', input_name)
+    input = Image.open(input_dir)
     result = model(input)
 
     result_img = Image.fromarray(result)
     result_img.save('result.png', format='PNG')
     
-run(model, input)
